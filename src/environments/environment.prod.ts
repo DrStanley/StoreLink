58889042f8745782634c871dabffa3430aca9902
Ozoezi Chukwujekwu Stanley <43678258+DrStanley@users.noreply.github.com>
2025-09-05 02:30:21 +0100

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
   production: true,
+  firebase:{
+  apiKey: "AIzaSyAzFuW51ho7UltlLe-7TPlp52nfPYe9cao",
+  authDomain: "storelink-943af.firebaseapp.com",
+  projectId: "storelink-943af",
+  storageBucket: "storelink-943af.firebasestorage.app",
+  messagingSenderId: "371239871227",
+  appId: "1:371239871227:web:839670ba906ae0ed2b065e",
+  measurementId: "G-SY478GQVFX"
+}
+
 };
